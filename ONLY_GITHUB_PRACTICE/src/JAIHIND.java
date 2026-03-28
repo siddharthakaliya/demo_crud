dceb7e5f75adce763333f306b77ed097040afdcf
Siddhartha Kaliya <siddharthakalia@gmail.com>
2026-03-28 22:46:58 +0530

    practice

---
+++ ONLY_GITHUB_PRACTICE/src/JAIHIND.java
@@ -0,0 +1,8 @@
+public class JAIHIND {
+    public static void main (String[] args){
+
+        String str= "Siddhartha";
+        String reversed= new StringBuilder(str).reverse().toString();
+        System.out.println("Reversed String :" +reversed);
+    }
+}
